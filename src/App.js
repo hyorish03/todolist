c5dfa283089842daeead782980b66f3e97b43336
hyorish03 <hyorish2001@gmail.com>
2022-08-16 17:04:25 +0900

    https://github.com/hyorish03/todolist.git

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, {Component} from "react";
 export default class App extends Component {
   render () {
     return (
-      <div> 안녕하세요. </div>
+      <div className="container">
+        <div className="todoBlock"></div>
+      </div>
     )
   }
 }
