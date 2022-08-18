302991a4fc9a6b5246e3da809085b86e355729fb
hyorish03 <hyorish2001@gmail.com>
2022-08-18 18:04:02 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
 import React, {Component} from "react";
-
+import "./App.css";
 export default class App extends Component {
+
+  state = {
+    todoData : [
+      {
+        id : 1,
+        title : '공부하기',
+        conpleted : true
+      },
+      {
+        id : 2,
+        title : '청소하기',
+        completed : false
+  
+      },
+    ],
+  }
+  btnStyle = {
+    color : "#fff",
+    border : "none",
+    padding : "5px 9px",
+    borderRadius : "50%",
+    cursor : "pointer",
+    float : "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding : "10px",
+      borderBottom : "1px #ccc dotted",
+      TextDecoderation : 'none'
+
+    }
+  }
+
+ 
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter(data => data.id !== id)
+    this.setState({ todoData : newTodoData});
+  }
+
   render () {
     return (
       <div className="container">
-        <div className="todoBlock"></div>
-      </div>
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+
+          {this.state.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id} >
+              <input type="checkbox" defaultChecked = {false} />
+              {data.title}
+              <button style={this.btnStyle} onClick={()=> this.handleClick(data.id)}>x</button>
+            </div>
+          ))} 
+            
+            <form style={{display : 'flex'}}>
+              <input 
+              type="text" 
+              name="value" 
+              style={{ flex : '10', padding : '5px'}}
+              placeholder="해야 할 일을 입력하세요."
+              value="" 
+              />
+
+              <input
+                type="submit"
+                value="입력"
+                className="btn"
+                style={{felx : 1}}
+              />
+
+            </form>
+
+            
+          </div>
+        </div>
+  
     )
   }
 }
